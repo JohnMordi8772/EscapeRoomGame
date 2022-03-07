f3eaa36a574627f244b0b2571c1425b2c605336f
tcolander <tcolander@mail.bradley.edu>
2022-03-07 13:30:14 -0600

    stuff

--- EscapeRoom/Assets/Scripts/TylerRoomRotate.cs
+++ EscapeRoom/Assets/Scripts/TylerRoomRotate.cs
@@ -8,6 +8,7 @@ public class TylerRoomRotate : MonoBehaviour
 
     public bool isHorizontal;
     public bool isVertical;
+    public bool isZertical;
     public GameObject GameController;
     public TylerGameController tgc;
 
@@ -27,6 +28,10 @@ public class TylerRoomRotate : MonoBehaviour
         if (transform.position.x == 0)
         {
             isVertical = true;
+        }
+        if(transform.position.z == 0)
+        {
+            isZertical = true;
         }
 
         if(tgc.canRotate == true)
@@ -123,7 +128,52 @@ public class TylerRoomRotate : MonoBehaviour
                     }
                 }
             }
+            /*if (Input.GetKeyDown(KeyCode.Z) && isZertical == true)
+            {
+                if (transform.position.y > 0)
+                {
+                    transform.position = new Vector3(0, 0, 5);
+                }
+                else if (transform.position.y < 0)
+                {
+                    transform.position = new Vector3(0, 0, -5);
+                }
+                else if (transform.position.y == 0)
+                {
+                    if (transform.position.z == 5)
+                    {
+                        transform.position = new Vector3(0, -5, 0);
+                    }
+                    else if (transform.position.z == -5)
+                    {
+                        transform.position = new Vector3(0, 5, 0);
+                    }
+                }
+            }
 
+            if (Input.GetKeyDown(KeyCode.X) && isZertical == true)
+            {
+                if (transform.position.y > 0)
+                {
+                    transform.position = new Vector3(0, 0, -5);
+                }
+                else if (transform.position.y < 0)
+                {
+                    transform.position = new Vector3(0, 0, 5);
+                }
+                else if (transform.position.y == 0)
+                {
+                    if (transform.position.z == -5)
+                    {
+                        transform.position = new Vector3(0, -5, 0);
+                    }
+                    else if (transform.position.z == 5)
+                    {
+                        transform.position = new Vector3(0, 5, 0);
+                    }
+                }
+            }
+            */
         }
     }    
         
